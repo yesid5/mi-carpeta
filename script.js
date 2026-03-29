4ad1f045a5dd665d9db8bf50f927c0f812e4183a
yesid5 <136249134+yesid5@users.noreply.github.com>
2026-03-29 14:35:41 -0500

    script.js
    Signed-off-by: yesid5 <136249134+yesid5@users.noreply.github.com>

--- script.js
+++ script.js
@@ -62,7 +62,7 @@ const POSApp = () => {
     
     // BARRA SUPERIOR
     React.createElement("nav", { className: "top-nav" },
-      React.createElement("div", { className: "nav-logo" }, "MercaExpress 33"),
+      React.createElement("div", { className: "nav-logo" }, "TIENDA JP"),
       React.createElement("div", { className: "nav-links" },
         React.createElement("button", { onClick: () => {setSeccion("tienda"); setAutenticado(false);}, className: seccion === "tienda" ? "active" : "" }, "🛒 Ventas"),
         React.createElement("button", { onClick: () => setSeccion("login"), className: seccion === "admin" || seccion === "login" ? "active" : "" }, "⚙️ Admin")
